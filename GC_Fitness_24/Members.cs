1d72aa815ce8a8466af2bcdabc8456bcd274181d
Ramon <rag4@pdx.edu>
2021-02-04 10:04:39 -0800

    initialized abstract Members class

--- GC_Fitness_24/Members.cs
+++ GC_Fitness_24/Members.cs
@@ -4,7 +4,27 @@ using System.Text;
 
 namespace GC_Fitness_24
 {
-    class Members
+    abstract class Members
     {
+        public string id { get; set; }
+        public string name { get; set; }
+
+        public Members(string Id, string Name)
+        {
+            this.id = Id;
+            this.name = Name;
+        }
+
+        public abstract void CheckIn(Club club);
+
+        public override string ToString()
+        {
+            string output = "";
+            output += $"Name: {this.name}\n";
+            output += $"Id: {this.id}\n";
+            return output;
+        }
     }
+
+     
 }
