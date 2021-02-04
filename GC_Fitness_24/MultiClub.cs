3c116dda69cfb8c2250d52f9af966f0a59fd5a83
Ramon <rag4@pdx.edu>
2021-02-04 10:31:55 -0800

    implemented MultiClub class

--- GC_Fitness_24/MultiClub.cs
+++ GC_Fitness_24/MultiClub.cs
@@ -6,6 +6,26 @@ namespace GC_Fitness_24
 {
     class MultiClub : Members
     {
-       // Multi-Club Members: a variable that stores their membership points.The CheckIn method adds to their membership points.*/
+        public int points { get; set; }
+        public MultiClub(string Name, string Id, int Points) : base(Name, Id)
+        {
+            this.points = Points;
+        }
+
+        // Multi-Club Members: a variable that stores their membership points.        
+        public override void CheckIn(Club club)
+        {
+            this.points += 100;
+        }
+
+        // The CheckIn method adds to their membership points.
+        public override string ToString()
+        {
+            string output = "";
+            output += $"Name: {this.name}\n";
+            output += $"Id: {this.id}\n";
+            output += $"Balance: {this.points} Points\n";
+            return output;
+        }
     }
 }
---
+++ GC_Fitness_24/MultiClubMembers.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GC_Fitness_24
+{
+    class MultiClubMembers
+    {
+    }
+}
